8293cdf31e1974d03dd3ac9e4c1b95977311b6f6
Sebastian Dowell <142616573+SebastianAPCS@users.noreply.github.com>
2024-04-20 18:56:52 +0000

    add StepTracker class

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,36 @@
 import java.util.ArrayList;
-public class StepTracker
-{
- /* to be implemented here */
-} 
+
+public class StepTracker {
+    ArrayList<Integer> steps;
+    int activeThreshold;
+
+    public StepTracker(int activeThreshold) {
+        this.activeThreshold = activeThreshold;
+        
+        steps = new ArrayList<>();
+    }
+
+    public void addDailySteps(int num) {
+        steps.add(Integer.valueOf(num));
+    }
+
+    public int activeDays() {
+        int count = 0;
+
+        for (Integer day : steps) {
+            count += day.intValue() >= activeThreshold ? 1 : 0;
+        }
+
+        return count;
+    }
+
+    public double averageSteps() {
+        double total = 0;
+
+        for (Integer day : steps) {
+            total += day.doubleValue();
+        }
+
+        return total / steps.size();
+    }
+}
